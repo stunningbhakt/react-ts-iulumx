fd61018a3db3108d8029cbb45e8369d4249d0c07
stunningbhakt <61114928+stunningbhakt@users.noreply.github.com>
2023-01-04 01:18:01 +0530

    Initial commit

---
+++ App.tsx
@@ -0,0 +1,7 @@
+import * as React from 'react';
+import AltCard from './columnCards';
+import './style.css';
+
+export default function App() {
+  return <AltCard />;
+}
---
+++ README.md
@@ -0,0 +1,3 @@
+# react-ts-iulumx
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/react-ts-iulumx)
